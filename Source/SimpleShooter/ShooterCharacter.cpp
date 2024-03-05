e5a88f7ede06aa4c58c69fb5221222895f5259ee
João Lucas <jony.lucas.br13@gmail.com>
2024-03-05 16:03:36 -0300

    - Character Movement

---
+++ Source/SimpleShooter/ShooterCharacter.cpp
@@ -0,0 +1,72 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ShooterCharacter.h"
+
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+
+// Sets default values
+AShooterCharacter::AShooterCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AShooterCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AShooterCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AShooterCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	PlayerController = Cast<APlayerController>(Controller);
+
+	if(PlayerController)
+	{
+		if(UEnhancedInputLocalPlayerSubsystem* LocalPlayerSubsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			LocalPlayerSubsystem->AddMappingContext(InputMappingContext, 0);
+		}
+	}
+
+	if(UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AShooterCharacter::Move);
+		EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &AShooterCharacter::Look);
+		EnhancedInputComponent->BindAction(FireAction, ETriggerEvent::Triggered, this, &AShooterCharacter::Fire);
+		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Triggered, this, &ACharacter::Jump);
+	}
+}
+
+void AShooterCharacter::Move(const FInputActionInstance& InputActionInstance)
+{
+	const FVector2d MoveValue = InputActionInstance.GetValue().Get<FVector2d>();
+	const FVector MoveDirection = MoveValue.Y * FVector::ForwardVector + MoveValue.X * FVector::RightVector;
+	AddActorLocalOffset(MoveDirection * MoveSpeed * GetWorld()->DeltaTimeSeconds, true);
+}
+
+void AShooterCharacter::Look(const FInputActionInstance& InputActionInstance)
+{
+	const FVector2d LookValue = InputActionInstance.GetValue().Get<FVector2d>();
+	AddControllerPitchInput(-LookValue.Y);
+	AddControllerYawInput(LookValue.X);
+}
+
+void AShooterCharacter::Fire(const FInputActionInstance& InputActionInstance)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Fire"));
+}
+
---
+++ Source/SimpleShooter/ShooterCharacter.h
@@ -0,0 +1,59 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "InputAction.h"
+#include "GameFramework/Character.h"
+#include "ShooterCharacter.generated.h"
+
+class UInputAction;
+class UInputMappingContext;
+
+UCLASS()
+class SIMPLESHOOTER_API AShooterCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AShooterCharacter();
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, Category="Input")
+	UInputMappingContext* InputMappingContext;
+
+	UPROPERTY(EditAnywhere, Category="Input")
+	UInputAction* MoveAction;
+
+	UPROPERTY(EditAnywhere, Category="Input")
+	UInputAction* LookAction;
+
+	UPROPERTY(EditAnywhere, Category="Input")
+	UInputAction* FireAction;
+
+	UPROPERTY(EditAnywhere, Category="Input")
+	UInputAction* JumpAction;
+
+private:
+	UPROPERTY(EditAnywhere, Category="Movement")
+	float MoveSpeed = 100.f;
+	
+	UPROPERTY(EditAnywhere, Category="Movement")
+	float RotationRate = 10.f;
+	
+	APlayerController* PlayerController;
+	void Move(const FInputActionInstance& InputActionInstance);
+	void Look(const FInputActionInstance& InputActionInstance);
+	void Fire(const FInputActionInstance& InputActionInstance);
+
+};
